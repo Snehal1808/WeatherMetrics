00a6be6a676cba4debf5ad96a29f61bd5113a649
Snehal1808 <snehalsubu18@gmail.com>
2025-12-31 18:21:48 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+API_KEY = "YOUR_OPENWEATHER_API_KEY"
+CITY = "Bhubaneswar"
+DB_CONFIG = {
+    "host": "localhost",
+    "user": "root",
+    "password": "your_password",
+    "database": "weather_db"
+}
